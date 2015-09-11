a355042ecb2cc4ab71a5c559ff77c1fd6f84157d
Ludwig Magnusson <ludwig@mediatool.com>
2015-09-11 11:27:25 +0200

    Expose more functions in index file

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+var findModule = require('./dist/find-module');
+var getConfigPaths = require('./dist/get-conf-paths');
+
 exports.fromConfig = require('./dist/load-config');
 exports.fromNodeConfig = require('./dist/load-node-config');
 exports.Container = require('./dist/container');
+exports.getConfigPaths = getConfigPaths(findModule.path, findModule.instance);
+exports.evaluateType = require('./dist/evaluate-type');
